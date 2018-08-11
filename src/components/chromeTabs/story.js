c7b382f34b0b4f3432ba58bebc74574da1e0606c
xiewang <1010658096@qq.com>
2018-08-11 23:33:37 +0800

    add more test cases

--- src/components/chromeTabs/story.js
+++ src/components/chromeTabs/story.js
@@ -5,5 +5,7 @@ import ChromeTabs from './index';
 
 
 storiesOf('chromeTabs', module)
-    .add('show', () => <ChromeTabs />)
+    .add('defalult', () => <ChromeTabs />)
+	.add('add tab', () => <ChromeTabs theme={'dark'}/>)
+	.add('delete current tab', () => <ChromeTabs theme={'dark'}/>)
 	.add('dark theme', () => <ChromeTabs theme={'dark'}/>);
